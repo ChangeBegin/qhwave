f5b9f686581221e1e2ed9a0a4ea98dd4d82866d0
li <12191640687@qq.com>
2017-10-27 14:58:53 +0800

    first commit

---
+++ QHWave/QHWave.m
@@ -0,0 +1,21 @@
+//
+//  QHWave.m
+//  QHWave
+//
+//  Created by lqh on 2017/10/27.
+//  Copyright © 2017年 li. All rights reserved.
+//
+
+#import "QHWave.h"
+
+@implementation QHWave
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
